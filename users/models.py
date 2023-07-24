13e1dd18383b5401ae372a712eed7b0699c6fa7b
StanSPbProger <103483644+spbproger@users.noreply.github.com>
2023-07-24 20:11:41 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     lat = models.DecimalField(max_digits=8, decimal_places=6, null=True)
     lng = models.DecimalField(max_digits=8, decimal_places=6, null=True)
 
@@ -24,7 +24,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, null=True)
     username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=30, min_length=10)
     role = models.CharField(max_length=9, default="member", choices=ROLE)
     age = models.PositiveIntegerField()
     locations = models.ManyToManyField(Location)
